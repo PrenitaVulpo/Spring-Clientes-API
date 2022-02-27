3665e9744021a8ea9ca861519eac2fa5bf0cd135
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-02-27 17:47:38 -0300

    Add DB config

---
+++ src/main/java/io/github/PrenitaVulpo/clientes/repository/ServiceRepository.java
@@ -0,0 +1,7 @@
+package io.github.PrenitaVulpo.clientes.repository;
+
+import io.github.PrenitaVulpo.clientes.model.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceRepository extends JpaRepository<Service, Integer> {
+}
