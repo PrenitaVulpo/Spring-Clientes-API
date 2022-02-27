dcb4cd6146184dc32527e97f5e74824f8ad00fdf
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-02-27 17:20:36 -0300

    Add Client Repository Interface

---
+++ src/main/java/io/github/PrenitaVulpo/clientes/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package io.github.PrenitaVulpo.clientes.repository;
+
+import io.github.PrenitaVulpo.clientes.model.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository  extends JpaRepository<Client, Integer> {
+}
