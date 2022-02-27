e38c6a728b1101c0c53f83f6e2ca8d186b6badd9
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-02-27 17:01:56 -0300

    Client class now have @Data annotation

--- src/main/java/io/github/PrenitaVulpo/clientes/model/entity/Client.java
+++ src/main/java/io/github/PrenitaVulpo/clientes/model/entity/Client.java
@@ -1,14 +1,12 @@
 package io.github.PrenitaVulpo.clientes.model.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
+@Data
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
